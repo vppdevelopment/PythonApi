5552d27cb15784f46dfe57800c5d87455a5a3942
maxneo4 <maxneo4@gmail.com>
2015-11-05 07:26:16 -0500

    Empezando a agregar manipulacion de lista de usuarios

--- vppApi.py
+++ vppApi.py
@@ -1,12 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from common.messages import User
 
 app = Flask(__name__)
+
+_users = []
 
 @app.route('/users')
 def getUsers():
     user = User()
     user.userName = 'maxneo'
     user.email = 'max@gmail.com'
+    _users.append(user.__dict__)
 
-    return jsonify(user.__dict__)
+    return jsonify(users=_users)
+
+@app.route('/user', methods=['POST'])
+def addUser():
+    json = request.json
+    print(json)
+    # Render template
+    return jsonify(json)
